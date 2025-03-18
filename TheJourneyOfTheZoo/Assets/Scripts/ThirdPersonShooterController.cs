29cd8a73af7c04cc45973349b09ad73b20d321ef
Shasho <zap580@amerike.edu.mx>
2025-03-17 18:51:09 -0600

    Aiming Methods Base
    Added crosshairs and basic logic to rotate character and aim, also implemented early bullet behaviour

---
+++ TheJourneyOfTheZoo/Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private GameObject normalCrosshair;
+    [SerializeField] private GameObject aimCrosshair;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
+    [SerializeField] private Transform pfBulletProjectile;
+    [SerializeField] private Transform spawnBulletPosition;
+
+    private ThirdPersonController thirdPersonController;
+    private StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        thirdPersonController = GetComponent<ThirdPersonController>();
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        //Can be removed once the top down mode is implemented!
+        aimCrosshair.SetActive(false);
+        normalCrosshair.SetActive(true);
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+        if (starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+            AimCrosshairController(true);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+            
+            //Rotate character to direction
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+            thirdPersonController.SetRotateOnMove(true);
+            AimCrosshairController(false);
+        }
+
+        if (starterAssetsInputs.shoot)
+        {
+            Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
+            Instantiate(pfBulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            starterAssetsInputs.shoot = false;
+        }
+        
+    }
+
+    private void AimCrosshairController(bool aimingNow)
+    {
+        if (aimingNow)
+        {
+            //normalCrosshair.SetActive(false);
+            aimCrosshair.SetActive(true);
+        }
+        else
+        {
+            aimCrosshair.SetActive(false);
+            //normalCrosshair.SetActive(true);
+        }
+    }
+}
