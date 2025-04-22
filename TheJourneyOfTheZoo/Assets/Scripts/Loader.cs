590705cca35ae4becd7f6f5c768db0dc891d1b69
PaTo81013 <patomaginniss@hotmail.com>
2025-04-21 21:53:08 -0600

    SceneManager with scene flow
    This Scenemanager contains a loading scene

---
+++ TheJourneyOfTheZoo/Assets/Scripts/Loader.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class Loader
+{
+    private class LoadingMonoBehaviour: MonoBehaviour { }
+    public enum Scene
+    {
+        LoadingScene,
+        Playground,
+        MainMenu
+    }
+
+    private static Action _onLoaderCallback;
+    private static AsyncOperation _loadingAsyncOperation;
+    
+    public static void Load(Scene scene)
+    {
+        _onLoaderCallback = () =>
+        {
+            GameObject loadingGameObject = new GameObject("Loading Game Object");
+            loadingGameObject.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+        };
+        
+        SceneManager.LoadSceneAsync(Scene.LoadingScene.ToString());
+    }
+
+    private static IEnumerator LoadSceneAsync(Scene scene)
+    {
+        yield return null;
+        _loadingAsyncOperation = SceneManager.LoadSceneAsync(scene.ToString());
+        while (!_loadingAsyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public static float GetLoadingProgress()
+    {
+        if (_loadingAsyncOperation !=null)
+        {
+            return _loadingAsyncOperation.progress;
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+    
+    public static void LoaderCallback ()
+    {
+        if (_onLoaderCallback == null) return;
+        _onLoaderCallback();
+        _onLoaderCallback = null;
+    }
+}
---
+++ TheJourneyOfTheZoo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        Loader.Load(Loader.Scene.Playground);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
