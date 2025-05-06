29fab90550b4e2f856b41803f7dce9c21d3a764f
PaTo81013 <patomaginniss@hotmail.com>
2025-05-06 14:47:42 -0600

    Made some prefabs

--- TheJourneyOfTheZoo/Assets/Scripts/Pause.cs
+++ TheJourneyOfTheZoo/Assets/Scripts/Pause.cs
@@ -1,17 +1,34 @@
+using TMPro.Examples;
 using UnityEngine;
 
 public class Pause : MonoBehaviour
 { 
     private static bool _ispaused = false;
     public GameObject pauseCanvas;
+    public GameObject CrosshairGameObject;
+    public MonoBehaviour cameraController;
+    public Canvas _TimerCanvas;
+    public Canvas _UsernameCanvas;
  
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
         {
             _ispaused = !_ispaused;
             Time.timeScale = _ispaused ? 0 : 1;
             pauseCanvas.SetActive(_ispaused);
+            CrosshairGameObject.SetActive(!_ispaused);
+            _TimerCanvas.enabled = !_ispaused;
+            _UsernameCanvas.enabled = !_ispaused;
+            
+            Cursor.lockState = _ispaused ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = _ispaused;
+            
+            if(cameraController != null)
+            {
+                cameraController.enabled = !_ispaused;
+            }
+
         }
     }
 }
