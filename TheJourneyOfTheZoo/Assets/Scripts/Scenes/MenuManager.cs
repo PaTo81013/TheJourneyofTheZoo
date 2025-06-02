c2fbb0914a87956dffc46d0f4be2d8af9ad0a356
PaTo81013 <patomaginniss@hotmail.com>
2025-06-02 14:44:03 -0600

    Enemies Nav mesh

--- TheJourneyOfTheZoo/Assets/Scripts/Scenes/MenuManager.cs
+++ TheJourneyOfTheZoo/Assets/Scripts/Scenes/MenuManager.cs
@@ -9,23 +9,27 @@ namespace Scenes
     
         public void Play()
         {
+            AudioManager.Instance.PlaySfx("Click");
             Loader.Load(Loader.Scene.Lobby);
         }
         
         public void Settings()
         {
+            AudioManager.Instance.PlaySfx("Click");
             mainMenuCanvas.SetActive(false);
             settingsCanvas.SetActive(true);
         }
     
         public void BackToMainMenu()
         {
+            AudioManager.Instance.PlaySfx("Click");
             mainMenuCanvas.SetActive(true);
             settingsCanvas.SetActive(false);
         }
 
         public void Credits()
         {
+            AudioManager.Instance.PlaySfx("Click");
             Loader.Load(Loader.Scene.Credits);
         }
         public void Quit()
